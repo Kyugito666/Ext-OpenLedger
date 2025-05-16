68484eec9d94ae9030b318e1ec63829e3e0e3044
Kyugito666 <113055222+Kyugito666@users.noreply.github.com>
2025-05-16 19:50:57 +0700

    Add files via upload

---
+++ content.bundle.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+/*!************************!*\
+  !*** ./src/content.js ***!
+  \************************/
+// chrome.runtime.requestUpdateCheck((status) => {
+//   if (status === "update_available") {
+//     // Reload the extension if an update is available
+//     chrome.runtime.reload();
+//   } else {
+//     console.log("Up to Date!");
+
+//     // If no update is available, show a message that the extension is up to date
+//     showMessage("Extension is up to date!");
+//   }
+// });
+/******/ })()
+;
+//# sourceMappingURL=content.bundle.js.map
